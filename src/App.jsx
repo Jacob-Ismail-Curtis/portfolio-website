839ef7364cd429a85e8bc476584cabc05afcbd7e
jcgit786 <jacobcurtis786@gmail.com>
2023-11-16 16:12:53 +0000

    Initial commit
    Initial project files setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Intro from './components/Intro'
+
+function App() {
+  return (
+    <div className="App">
+      <Intro />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Intro.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Intro() {
+  return (
+    <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+        <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Jacob Curtis</h1>
+        <p className="text-base md:text-xl mb-3 font-medium ">Full-stack Software Engineer</p>
+        <p className="text-sm max-w-xl mb-6 font-bold">I am a full-stack software engineer with a BSc in Computer Science from Durham University. </p>
+    </div>
+  ) 
+}
+
+export default Intro;
